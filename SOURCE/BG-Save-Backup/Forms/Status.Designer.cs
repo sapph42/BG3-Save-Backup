054b3d5db4b60e2a3dfdaa54865dea42fdeec8b4
sapph42 <31635854+sapph42@users.noreply.github.com>
2024-11-25 09:18:31 -0500

    Add click event handler for dgvc

--- SOURCE/BG-Save-Backup/Forms/Status.Designer.cs
+++ SOURCE/BG-Save-Backup/Forms/Status.Designer.cs
@@ -158,6 +158,7 @@ namespace BG3_Save_Backup.Forms {
             this.SavesDgv.Size = new System.Drawing.Size(643, 207);
             this.SavesDgv.TabIndex = 8;
             this.SavesDgv.MouseUp += new System.Windows.Forms.MouseEventHandler(this.SavesDgv_MouseUp);
+            this.SavesDgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.SavesDgv_CellClick);
             // 
             // SavePath
             // 
